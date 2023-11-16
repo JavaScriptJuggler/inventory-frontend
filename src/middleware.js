94a5f78fc1fae52d148689d31e63511287fd439b
Soumya Manna <darkprience180898@gmail.com>
2023-11-16 20:59:01 +0530

    middleware logic change

--- src/middleware.js
+++ src/middleware.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 export function middleware(request) {
     // console.log(request.cookies.get('token'));return
-    if (request.nextUrl.pathname === '/') {
+    if (request.nextUrl.pathname == '/') {
         let token = request.cookies.get('token');
         let user_data = request.cookies.get('user_data');
         if (token && user_data)
